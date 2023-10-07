221a64e6612856f602939c922a064b4dcb4c9fb0
mahesh-babu-drivex <mahesh.babu@drivex.in>
2023-10-07 19:30:54 +0530

    chnages

--- models/example/my_model.sql
+++ models/example/my_model.sql
@@ -13,7 +13,7 @@
 
 -- Reference the existing table and include all columns in a CTE
 WITH SHEET1 AS (
-  SELECT * FROM {{ ref('AIRBYTE_DATABASEE.AIRBYTE_SCHEMAA.SHEET1') }}
+  SELECT * FROM {{ ref('AIRBYTE_SCHEMAA.SHEET1') }}
 )
 
 -- You can now use the source_data CTE in your dbt model
